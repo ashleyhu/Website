46d11a11a68c21c5c6949f1fcfab33ec06e4572a
ashleyhu <ashleyhu97@gmail.com>
2016-05-13 00:53:13 -0400

    Personal Portfolio Website

---
+++ js/cbpAnimatedHeader2.js
@@ -0,0 +1,23 @@
+var cbpAnimatedHeader = (function() {
+
+	var docElem = document.documentElement,
+		header = document.querySelector( '.navbar-default' ),
+		didScroll = false,
+		changeHeaderOn = 300;
+
+	function init() {
+		window.addEventListener( 'scroll', function( event ) {
+			if( !didScroll ) {
+				didScroll = true;
+				setTimeout( scrollPage, 250 );
+			}
+		}, false );
+	}
+
+	function scrollY() {
+		return window.pageYOffset || docElem.scrollTop;
+	}
+
+	init();
+
+})();
---
+++ js/script.js
@@ -0,0 +1,81 @@
+
+//SMOOTH PAGE SCROLL
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+  $("#owl-carousel").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 4,
+      pagination: false,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+
+  $("#owl-slider").owlCarousel({
+      navigation : false,
+      pagination : false, 
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true,
+      
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+
+  });
+
+
+
+
+$(document).ready(
+
+  function() { 
+
+    $("html").niceScroll({
+    	cursorcolor:"#000",
+    	scrollspeed :"30",
+    	cursorborder:"1px solid #000",
+    	horizrailenabled: "false",
+    	cursorborderradius: "0px"
+    });
+    
+    // portfolio filtering
+
+    $(".project-wrapper").mixItUp();
+
+});
+
+new WOW().init();
+
+    
+/*Preloader*/
+//<![CDATA[
+$(window).load(function() { // makes sure the whole site is loaded
+  $('#status').fadeOut(); // will first fade out the loading animation
+  $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+  $('body').delay(350).css({'overflow':'visible'});
+  $('.navbar-right').delay(350).css({'visibility':'visible'});
+})
+//]]>
+
+
