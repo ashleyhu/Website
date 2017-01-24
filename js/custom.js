edda1a2e65b44bcc45e795dfd7eb0caf6c711052
ashleyhu <ashleyhu97@gmail.com>
2017-01-24 07:53:18 -0500

    2017update

---
+++ js/custom.js
@@ -0,0 +1,287 @@
+(function($){
+
+	"use strict";
+
+	/* ---------------------------------------------- /*
+	 * Preloader
+	/* ---------------------------------------------- */
+
+	$(window).load(function() {
+		$('.loader').fadeOut();
+		$('.page-loader').delay(350).fadeOut('slow');
+	});
+
+	$(document).ready(function() {
+
+		/* ---------------------------------------------- /*
+		 * Initialization general scripts for all pages
+		/* ---------------------------------------------- */
+
+		var moduleHero      = $('#hero'),
+			overlayMenu     = $('#overlay-menu'),
+			slider          = $('#slides'),
+			navbar          = $('.navbar-custom'),
+			filters         = $('#filters'),
+			worksgrid       = $('#works-grid'),
+			modules         = $('.module-hero, .module, .module-small'),
+			windowWidth     = Math.max($(window).width(), window.innerWidth),
+			navbatTrans,
+			mobileTest;
+
+		navbarCheck(navbar);
+
+		$(window).resize(function() {
+			buildModuleHero();
+		});
+
+		$(window).scroll(function() {
+			navbarAnimation(navbar, moduleHero);
+		}).scroll();
+
+		/* ---------------------------------------------- /*
+		 * Mobile detect
+		/* ---------------------------------------------- */
+
+		if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+			mobileTest = true;
+		} else {
+			mobileTest = false;
+		}
+        
+		/* ---------------------------------------------- /*
+		 * Setting background of modules
+		/* ---------------------------------------------- */
+
+		modules.each(function() {
+			if ($(this).attr('data-background')) {
+				$(this).css('background-image', 'url(' + $(this).attr('data-background') + ')');
+			}
+		});
+
+		/* ---------------------------------------------- /*
+		 * Full height module
+		/* ---------------------------------------------- */
+
+		function buildModuleHero() {
+			if (moduleHero.length > 0) {
+				if (moduleHero.hasClass('module-full-height')) {
+					moduleHero.height($(window).height());
+				} else {
+					moduleHero.height($(window).height() * 0.85);
+				}
+			}
+		}
+
+		/* ---------------------------------------------- /*
+		 * Transparent navbar animation
+		/* ---------------------------------------------- */
+
+		function navbarCheck() {
+			if (navbar.length > 0 && navbar.hasClass('navbar-transparent')) {
+				navbatTrans = true;
+			} else {
+				navbatTrans = false;
+			}
+		}
+
+		function navbarAnimation(navbar, moduleHero) {
+			var topScroll = $(window).scrollTop();
+			if (navbar.length > 0 && navbatTrans !== false) {
+				if (topScroll >= 5) {
+					navbar.removeClass('navbar-transparent');
+				} else {
+					navbar.addClass('navbar-transparent');
+				}
+			}
+		}
+
+		/* ---------------------------------------------- /*
+		 * Portfolio
+		/* ---------------------------------------------- */
+
+		$('a', filters).on('click', function() {
+			var selector = $(this).attr('data-filter');
+
+			$('.current', filters).removeClass('current');
+			$(this).addClass('current');
+
+			worksgrid.isotope({
+				filter: selector
+			});
+
+			return false;
+		});
+
+		$(window).on('resize', function() {
+
+			var windowWidth    = Math.max($(window).width(), window.innerWidth),
+				itemWidht      = $('.grid-sizer').width(),
+				itemHeight     = Math.floor(itemWidht * 0.95),
+				itemTallHeight = itemHeight * 2;
+
+			if (windowWidth > 500) {
+				$('.work-item', worksgrid).each(function() {
+					if ($(this).hasClass('tall')) {
+						$(this).css({
+							height : itemTallHeight
+						});
+					} else if ($(this).hasClass('wide')) {
+						$(this).css({
+							height : itemHeight
+						});
+					} else if ($(this).hasClass('wide-tall')) {
+						$(this).css({
+							height : itemTallHeight
+						});
+					} else {
+						$(this).css({
+							height : itemHeight
+						});
+					}
+				});
+			} else {
+				$('.work-item', worksgrid).each(function() {
+					if ($(this).hasClass('tall')) {
+						$(this).css({
+							height : itemTallHeight
+						});
+					} else if ($(this).hasClass('wide')) {
+						$(this).css({
+							height : itemHeight / 2
+						});
+					} else if ($(this).hasClass('wide-tall')) {
+						$(this).css({
+							height : itemHeight
+						});
+					} else {
+						$(this).css({
+							height : itemHeight
+						});
+					}
+				});
+			}
+
+		}).resize();
+
+		/* ---------------------------------------------- /*
+		 * Ajax options
+		/* ---------------------------------------------- */
+
+		var pageNumber = 0,
+			workNumberToload = 5;
+
+		var doneText    = 'Done',
+			loadText    = 'More works',
+			loadingText = 'Loading...',
+			errorText   = 'Error! Check the console for more information.';
+
+		/* ---------------------------------------------- /*
+		 * Ajax portfolio
+		/* ---------------------------------------------- */
+
+		$('#show-more').on('click', function() {
+			$(this).text(loadingText);
+
+			setTimeout(function() {
+				ajaxLoad(workNumberToload, pageNumber);
+			}, 300);
+
+			pageNumber++;
+
+			return false;
+		});
+
+		function ajaxLoad(workNumberToload, pageNumber) {
+			var $loadButton = $('#show-more');
+			var dataString = 'numPosts=' + workNumberToload + '&pageNumber=' + pageNumber;
+
+			$.ajax({
+				type: 'GET',
+				data: dataString,
+				dataType: 'html',
+				url: 'assets/php/ajax-load-more/ajax-load-more.php',
+				success: function(data) {
+					var $data = $(data);
+					var start_index = (pageNumber - 1) * workNumberToload;
+					var end_index = + start_index + workNumberToload;
+
+					if ($data.find('.work-item').slice(start_index).length) {
+						var work = $data.find('.work-item').slice(start_index, end_index);
+
+						worksgrid.append(work).isotope('appended', work).resize();
+
+						setTimeout(function() {
+							$loadButton.text(loadText);
+						}, 300);
+					} else {
+						setTimeout(function() {
+							$loadButton.text(doneText);
+						}, 300);
+
+						setTimeout(function () {
+							$('#show-more').animate({
+								opacity: 0,
+							}).css('display', 'none');
+						}, 1500);
+					}
+				},
+
+				error: function (jqXHR, textStatus, errorThrown) {
+					console.log(jqXHR + " :: " + textStatus + " :: " + errorThrown);
+
+					setTimeout(function() {
+						$loadButton.removeClass('ss-loading');
+						$loadButton.text(errorText);
+					}, 300);
+
+				}
+			});
+		}
+
+		/* ---------------------------------------------- /*
+		 * Owl sliders
+		/* ---------------------------------------------- */
+
+		$('.slider-testimonials').owlCarousel({
+			stopOnHover:     !0,
+			singleItem:      !0,
+			autoHeight:      !0,
+			slideSpeed:      400,
+			paginationSpeed: 1000,
+			goToFirstSpeed:  2000,
+			autoPlay:        3000,
+			navigationText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+		});
+
+		$('.slider-images').owlCarousel({
+			stopOnHover:     !0,
+			singleItem:      !0,
+			autoHeight:      !0,
+			navigation:      !0,
+			slideSpeed:      400,
+			paginationSpeed: 1000,
+			goToFirstSpeed:  2000,
+			autoPlay:        3000,
+			navigationText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+		});
+
+		/* ---------------------------------------------- /*
+		 * Gallery
+		/* ---------------------------------------------- */
+
+		$('a.gallery').magnificPopup({
+			type: 'image',
+			gallery: {
+				enabled: true,
+				navigateByImgClick: true,
+				preload: [0,1]
+			},
+			image: {
+				titleSrc: 'title',
+				tError: 'The image could not be loaded.',
+			}
+		});
+
+	});
+
+})(jQuery);
--- js/script.js
+++ js/script.js
@@ -74,6 +74,7 @@ $(window).load(function() { // makes sure the whole site is loaded
   $('#status').fadeOut(); // will first fade out the loading animation
   $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
   $('body').delay(350).css({'overflow':'visible'});
+  $('.navbar.navbar-default.navbar-fixed-top').delay(350).css({'visibility':'visible'});
   $('.navbar-right').delay(350).css({'visibility':'visible'});
 })
 //]]>
